33a6e20477f95c6ff2ca0eedca4a838ed8912a94
sai <saidhakshin75@gmail.com>
2021-09-02 22:36:55 +0530

    Initial commit.

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.set('view engine','ejs');
+
+app.get("/",async (req,res)=>{
+    res.render('mail');
+})
+
+app.post('/mail', async(req,res)=>{
+    console.log(req.body);
+    let transporter = await nodemailer.createTransport({
+        service : "gmail",
+        port : 467,
+        secure : true,
+        auth : {
+            user : 'saidhakshin75@gmail.com',
+            pass : 'qmpzFGH4563',
+        },
+    });
+    
+    let options = {
+        to : req.body.email,
+        from : 'saidhakshin75@gmail.com',
+        subject : req.body.message,
+        text : 'Hello',
+        html : "<h2>Hello</h2>"
+    }
+
+    await transporter.sendMail(options , (err,info) => {
+        if(err){
+            console.log(err);
+        }
+        console.log(info.response);
+    })
+
+    res.redirect("/");
+})
+
+app.listen(port , () => {
+    console.log(`Started at port ${port}`);
+})
